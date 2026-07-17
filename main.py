f311a45e91c1ce600b85c26dace20042f88f51b9
hiesakkiraj-collab <hiesakkiraj@gmail.com>
2026-07-17 13:13:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
-from dhanhq import marketfeed
+from dhanhq.marketfeed import DhanFeed  # இந்த வரியை மட்டும் கவனமாக மாற்றவும்
 
 # Render Environment Variables-ல் இருந்து விபரங்களை எடுக்கும்
 client_id = os.environ.get("DHAN_CLIENT_ID")
 access_token = os.environ.get("DHAN_ACCESS_TOKEN")
 
-# இது தான் தனுஷ் கொடுக்கும் அதிகாரப்பூர்வ வழி
 def get_live_market_data():
     # 3045 = SBIN (NSE Equity)
-    instruments = [("NSE_EQ", 3045)]
+    # இதில் 'NSE_EQ' என செக்மென்ட்டை நேரடியாக ஸ்டிரிங்காக கொடுப்போம்
+    instruments = [("NSE_EQ", 3045)] 
     
     def on_connect(instance):
         instance.subscribe(instruments)
@@ -17,7 +17,8 @@ def get_live_market_data():
     def on_message(instance, message):
         print("RECEIVED DATA:", message)
 
-    dhan = marketfeed.DhanFeed(client_id, access_token, instruments, on_connect, on_message)
+    # நேரடியாக DhanFeed-ஐ பயன்படுத்துகிறோம்
+    dhan = DhanFeed(client_id, access_token, instruments, on_connect, on_message)
     dhan.run_forever()
 
 if __name__ == "__main__":
